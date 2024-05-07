e5989a664008063bf9c9195058b4e7ea5f3ef4f7
Rishat Dilmurat (Rexiati Dilimulati) <32851626+UlughBeg86@users.noreply.github.com>
2024-05-07 12:14:49 +0200

    It starts

---
+++ test.sh
@@ -0,0 +1,7 @@
+echo "# DataScience-may-2024_ErdosInstitute" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/UlughBeg86/DataScience-may-2024_ErdosInstitute.git
+git push -u origin main
